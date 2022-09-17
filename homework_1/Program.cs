7512a4860c3dadede445224cc7678bc997f768e9
ElenaZ1985 <zima_szn@list.ru>
2022-09-17 18:30:25 +0400

    Урок1

---
+++ homework_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// 
+Console.WriteLine("Введите число A");
+string input = Console.ReadLine();
+int a = int.Parse(input);
+Console.WriteLine("Введите число B");
+string input2 = Console.ReadLine();
+int b = int.Parse(input2);
+int max = 0;
+int min = 0;
+{
+if (a > b) max = a;
+if (a > b) min = b;
+if (b > a) max = b;
+if (b > a) min = a;
+}
+Console.WriteLine("Большее числа =");
+Console.WriteLine(max);
+Console.WriteLine("Меньшее число =");
+Console.WriteLine(min);
