4bb1fc9c2205b7c52678c8aff745ca14fa3acf05
ElenaZ1985 <zima_szn@list.ru>
2022-09-18 10:13:56 +0400

    Урок2

---
+++ homework_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int[] numbers = { a, b };
+
+int min = numbers.Min();
+int max = numbers.Max();
+Console.WriteLine($"Min: {min}");
+Console.WriteLine($"Max: {max}");
