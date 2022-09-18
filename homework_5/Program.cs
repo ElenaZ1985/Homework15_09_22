7f11dfc8092a1ad214d2f7b186bd59fe1b1fbad7
ElenaZ1985 <zima_szn@list.ru>
2022-09-18 11:46:16 +0400

    Урок5

---
+++ homework_5/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Напишите программу, которая на вход принимает число (N > 0), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите положительное число");
+int num = int.Parse(Console.ReadLine());
+
+int count =2;
+
+while (count <= num)
+{
+    Console.WriteLine($"{count}");
+    count = count + 2;
+}    
